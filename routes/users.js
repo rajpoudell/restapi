0e69251a8887d5282118d5d88d83e22806101add
Raj Poudel <raj.poudeel@gmail.com>
2023-12-23 09:38:21 +0545

    using postman

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,31 @@
-import { Express } from "express";
+import express  from "express";
 
 const router  = express.Router();
 
-router.get("/users", (req,res) => {
-    res.send('Hello from users routes')
+
+const users = [ 
+        {
+            fName  : "Raj" ,
+            lName  : "Poudel" ,
+            age : 34 
+        },
+        {
+            fName  : "rojina" ,
+            lName  : "Poudel" ,
+            age : 34 
+        }
+]
+
+//all routes in here are starting with /users
+router.get('/', (req,res) => {
+    res.send(users)   
+});
+
+router.post('/',(req,res) =>{
+    console.log("post router reached");
+    const user  = req.body //post req on postman wala nabin pandey
+
+    // users.push()
 });
 
 
