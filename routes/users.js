270cf0c61d76cf0082e42554cc339fb8f19a3d99
Raj Poudel <raj.poudeel@gmail.com>
2023-12-24 08:00:46 +0545

    geting id

--- routes/users.js
+++ routes/users.js
@@ -11,18 +11,19 @@ router.get('/', (req,res) => {
     res.send(users)   
 });
 
-router.post('/',(req,res) =>{
-    const user  = req.body //post req on postman wala nabin pandey
-    
-    // const userId = uuidv4() ; // ⇨ '9b1deb4d-3b7d-4bad-9bdd-2b0d7b3dcb6d'
-    const userWithId = { ...user , id :  uuidv4()}
-//creating shortcut with directly using userid and userWithId in push method
+router.post('/', (req, res) =>{
+    const user  = req.body //post req on postman wala nabin pandey  
+ 
+    users.push({ ...user, id:  uuidv4() }) ;
 
-    users.push(userWithId);
-
-    res.send(`User with the name ${user.fName} added to the datebase !`);
-    console.log(`User with the name ${user.fName} added to the datebase !`);
+    res.send(`User with the name ${user.firstName} added to the datebase !`);
 });
+
+router.get('/:id' , (req,res)  => {
+    const {id}  = req.params;
+    const foundUser = users.find( (user) => user.id===id);
+    res.send( foundUser)
+})
 
 
 export default router;
